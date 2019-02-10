fdd754f592b64aaef668a82593df8a411564daca
Jake Herrmann <jtherrmann@alaska.edu>
2019-02-09 20:35:32 -0900

    Finish pa2.lua

--- hw2/pa2.lua
+++ hw2/pa2.lua
@@ -1,11 +1,19 @@
+-- pa2.lua
+-- Jake Herrmann
+-- 9 Feb 2019
+-- CS 331 Spring 2019
+--
+-- pa2 module for Assignment 2.
+
+
 local pa2 = {}
 
 
--- TODO:
--- - re-read project reqs
--- - follow coding standards
-
-
+-- pa2.mapTable
+--
+-- Return the table that results from mapping the given function over the
+-- values in the given table.
+--
 -- Pre:
 -- - f is a one-parameter function that accepts any value in table t.
 function pa2.mapTable(f, t)
@@ -17,15 +25,20 @@ function pa2.mapTable(f, t)
 end
 
 
+-- pa2.concatMax
+--
+-- Return the string that is the concatenation of as many copies of the given
+-- string as possible, without the length exceeding the given integer.
+--
+-- Pre:
+-- - str is a string.
+-- - maxLen is an integer.
 function pa2.concatMax(str, maxLen)
-   local concatStr = ""
-   while #concatStr + #str <= maxLen do
-      concatStr = concatStr .. str
-   end
-   return concatStr
+   return string.rep(str, math.floor(maxLen / #str))
 end
 
 
+-- The Collatz function.
 local function c(n)
    if n % 2 == 0 then
       return n / 2
@@ -35,6 +48,13 @@ local function c(n)
 end
 
 
+-- pa2.collatz
+--
+-- Return an iterator that produces the Collatz sequence starting at the given
+-- integer.
+--
+-- Pre:
+-- - k is a positive integer.
 function pa2.collatz(k)
    local last = nil
 
@@ -54,12 +74,17 @@ function pa2.collatz(k)
 end
 
 
+-- pa2.backSubs
+--
+-- Yield all substrings of the reverse of the given string.
+--
+-- Pre:
+-- - s is a string.
 function pa2.backSubs(s)
    local revStr = string.reverse(s)
-   local length = #revStr
    coroutine.yield("")
-   for offset = 0, length - 1 do
-      for index = 1, length - offset do
+   for offset = 0, #revStr - 1 do
+      for index = 1, #revStr - offset do
 	 coroutine.yield(string.sub(revStr, index, index + offset))
       end
    end
