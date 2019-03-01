033aee2dd56956d8b6f16e78f5b2acaf4d0fcbe7
Jake Herrmann <jtherrmann@alaska.edu>
2019-02-28 15:37:15 -0900

    Use lexit in parseit.lua

--- hw3-4/parseit.lua
+++ hw3-4/parseit.lua
@@ -1,9 +1,38 @@
 local parseit = {}
 
+local lexit = require "lexit"
+
+local lexIter
+local lexStr
+local lexCat
+
+
+local function lexNext()
+   lexStr, lexCat = lexIter()
+end
+
+
+local function lexInit(input)
+   lexIter = lexit.lex(input)
+   lexNext()
+end
+
+
+-- AST constants
 local STMT_LIST = 1
 
-function parseit.parse()
+local parseProgram
+
+
+function parseit.parse(input)
+   lexInit(input)
+   return parseProgram()
+end
+
+
+function parseProgram()
    return true, true, {STMT_LIST}
 end
+   
 
 return parseit
