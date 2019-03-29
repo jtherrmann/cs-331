6834ef345e39825c36610180bb5ae91b85d27e5b
Jake Herrmann <jtherrmann@alaska.edu>
2019-03-28 19:53:18 -0800

    Comment median.hs

--- haskell/median.hs
+++ haskell/median.hs
@@ -13,29 +13,43 @@ import Data.List
 import System.IO
 
 
+-- median
+--
+-- Return the median of the given list of numbers, or Nothing if the list is
+-- empty.
 median [] = Nothing
 median xs = Just (sort xs !! div (length xs) 2)
 
 
+-- medianStr
+--
+-- Return a string that displays the given median.
 medianStr Nothing       = "Empty list - no median"
 medianStr (Just median) = "Median: " ++ show median
 
 
--- TODO: rename
-loop :: [Integer] -> IO()
-loop xs = do
+-- medianLoop
+--
+-- Given the current list of numbers, prompt the user to enter another number
+-- or a blank line. If they enter a blank line, print the median of the given
+-- list. Otherwise add the input number to the list and continue the loop.
+medianLoop :: [Integer] -> IO()
+medianLoop xs = do
   putStr "Enter number (blank line to end): "
   hFlush stdout
   line <- getLine
   if line == ""
     then putStrLn $ medianStr $ median xs
-    else loop $ read line : xs
+    else medianLoop $ read line : xs
 
 
+-- main
+--
+-- Allow the user to compute medians of lists of numbers.
 main = do
   putStrLn "Enter a list of integers, one on each line."
   putStrLn "I will compute the median of the list.\n"
-  loop []
+  medianLoop []
 
   putStr "\nCompute another median? [y/n] "
   hFlush stdout
